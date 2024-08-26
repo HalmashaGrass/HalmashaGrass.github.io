164acd94945bc12ef2e8b21ef1e1c1ea54fc2391
eytangut <eytangut@gmail.com>
2024-08-26 18:13:28 +0300

    Revert "m"
    This reverts commit d850414b1924b01eda0bb7181122097e3335b593.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>,
+  </StrictMode>,
 )
