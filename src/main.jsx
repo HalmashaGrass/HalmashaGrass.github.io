d850414b1924b01eda0bb7181122097e3335b593
eytangut <eytangut@gmail.com>
2024-08-26 18:08:56 +0300

    m

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>,
+  </HashRouter>,
 )
